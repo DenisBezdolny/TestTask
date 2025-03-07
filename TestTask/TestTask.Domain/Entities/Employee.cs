9d7675f28ac03550b4c09f5b1671b9a19dacabc5
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-03-08 00:15:14 +0300

    Added domain layer Added Employee entity Added Interfaces's plugs

---
+++ TestTask/TestTask.Domain/Entities/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace TestTask.Domain.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; private set; }
+
+        public string PayrollNumber { get; set; }
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string MobilePhoneNumber { get; set; }
+        public string StreetAddress { get; set; }
+        public string Settlement { get; set; }
+        public string Postcode { get; set; }
+        public string Email { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
