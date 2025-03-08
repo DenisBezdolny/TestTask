103352c30effde68b79f01d3264071d9df5aab91
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-03-09 00:26:37 +0300

    Created Employee import view model

---
+++ TestTask/TestTask/Models/EmployeeImportViewModel.cs
@@ -0,0 +1,10 @@
+﻿using TestTask.Domain.Entities;
+
+namespace TestTask.Models
+{
+    public class EmployeeImportViewModel
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+        public string Message { get; set; }
+    }
+}
