268f869e6f3545aa6c06a7b91f1473a457101589
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-03-08 19:40:03 +0300

    Added factory's implementation

---
+++ TestTask/TestTask.BLL/Factories/EmployeeFactory.cs
@@ -0,0 +1,38 @@
+﻿using TestTask.Domain.Entities;
+using TestTask.Domain.Interfaces.Fabrics;
+
+namespace TestTask.BLL.Factories
+{
+    public class EmployeeFactory : IEmployeeFactory
+    {
+        public Employee CreateEmployee(
+            string payrollNumber,
+            string forename,
+            string surname,
+            DateTime dateOfBirth,
+            string phoneNumber,
+            string mobilePhoneNumber,
+            string streetAddress,
+            string settlement,
+            string postcode,
+            string email,
+            DateTime startDate)
+        {
+            
+            return new Employee
+            {
+                PayrollNumber = payrollNumber,
+                Forename = forename,
+                Surname = surname,
+                DateOfBirth = dateOfBirth,
+                PhoneNumber = phoneNumber,
+                MobilePhoneNumber = mobilePhoneNumber,
+                StreetAddress = streetAddress,
+                Settlement = settlement,
+                Postcode = postcode,
+                Email = email,
+                StartDate = startDate
+            };
+        }
+    }
+}
