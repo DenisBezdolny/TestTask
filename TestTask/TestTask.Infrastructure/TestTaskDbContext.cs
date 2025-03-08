c14a6b9b2ca3603bf8806acf1aa11baa615c1c47
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-03-08 13:11:26 +0300

    Created DbContext and it's factory

---
+++ TestTask/TestTask.Infrastructure/TestTaskDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Domain.Entities;
+using TestTask.Infrastructure.Configurations;
+
+namespace TestTask.Infrastructure
+{
+    public class TestTaskDbContext : DbContext 
+    {
+        public DbSet<Employee?> Employees { get; set; }
+
+        public TestTaskDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+        }
+                
+    }
+}
