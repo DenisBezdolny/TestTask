1dd70e43d1907fa10422b6e8563698584e97f728
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2025-03-08 14:33:55 +0300

    Implemented employee import interface

--- TestTask/TestTask.BLL/Services/EmployeeImportService.cs
+++ TestTask/TestTask.BLL/Services/EmployeeImportService.cs
@@ -1,12 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CsvHelper;
+using TestTask.Domain.Interfaces.Fabrics; // Consider renaming to IEmployeeFactory if appropriate
+using TestTask.Domain.Interfaces.Repositories;
+using TestTask.Domain.Interfaces.BLL;
+using TestTask.Domain.Entities;
 
 namespace TestTask.BLL.Services
 {
-    internal class EmployeeImportService
+    public class EmployeeImportService : IEmployeeImportService
     {
+        private readonly IEmployeeFactory _employeeFactory;
+        private readonly IRepository<Employee> _employeeRepository;
+
+        public EmployeeImportService(IEmployeeFactory employeeFacrory, IRepository<Employee> employeeRepository)
+        {
+            _employeeFactory = employeeFacrory;
+            _employeeRepository = employeeRepository;
+        }
+
+        public async Task<int> ImportEmployeeAsync(CsvReader csvReader)
+        {
+            int successCount = 0;
+
+            // Register the mapping if you're not using auto-mapping via attributes:
+            csvReader.Context.RegisterClassMap<EmployeeMap>();
+
+            while (csvReader.Read())
+            {
+                try
+                {
+                    var employee = _employeeFactory.CreateEmployee(
+                        csvReader.GetField("Personnel_Records.Payroll_Number"),
+                        csvReader.GetField("Personnel_Records.Forenames"),
+                        csvReader.GetField("Personnel_Records.Surname"),
+                        DateTime.ParseExact(csvReader.GetField("Personnel_Records.Date_of_Birth"), "dd/MM/yyyy", null),
+                        csvReader.GetField("Personnel_Records.Telephone"),
+                        csvReader.GetField("Personnel_Records.Mobile"),
+                        csvReader.GetField("Personnel_Records.Address"),
+                        csvReader.GetField("Personnel_Records.Address_2"),
+                        csvReader.GetField("Personnel_Records.Postcode"),
+                        csvReader.GetField("Personnel_Records.EMail_Home"),
+                        DateTime.ParseExact(csvReader.GetField("Personnel_Records.Start_Date"), "dd/MM/yyyy", null)
+                    );
+
+                    await _employeeRepository.CreateAsync(employee);
+                    successCount++;
+                }
+                catch (Exception ex)
+                {
+                    // TODO: Log the exception for debugging purposes.
+                    // Example: _logger.LogError(ex, "Error importing employee record.");
+                }
+            }
+
+            return successCount;
+        }
     }
 }
